1086cbaf9c741da56506a6eab30f2a0a71a145c4
alumno <alumno@Instituto.local>
2025-03-18 09:46:28 -0300

    dadas

--- js.js
+++ js.js
@@ -2,3 +2,9 @@ let anoNacimiento=2008
 let anoActual=2025
 let edad=anoActual - anoNacimiento
 alert("Mi edad es "+edad)
+let edadd=17
+if(edadd>=17){
+alert("Podes pasar master pero esta vez nomas")
+}else{
+    alert("Poli hay un menor agarrenlo")
+}
