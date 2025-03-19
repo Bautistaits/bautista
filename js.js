917dcadb35dd6b39cfa819f773ded01383903c98
alumno <alumno@Instituto.local>
2025-03-19 08:16:07 -0300

    dada

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let anoNacimiento=2008
+/*let anoNacimiento=2008
 let anoActual=2025
 let edad=anoActual - anoNacimiento
 alert("Mi edad es "+edad)
@@ -7,4 +7,14 @@ if(edadd>=17){
 alert("Podes pasar master pero esta vez nomas")
 }else{
     alert("Poli hay un menor agarrenlo")
+}*/
+let ContraseñaCorrecta = true
+let GmailCorrecto = false
+
+
+if((ContraseñaCorrecta) && (GmailCorrecto))
+{
+alert("Bienvenido")
+}else{
+    alert("ERROR")
 }
